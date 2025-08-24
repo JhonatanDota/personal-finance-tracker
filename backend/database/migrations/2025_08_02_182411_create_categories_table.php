344b9c77b6e3fc309da7f81ae604f279ee4129f7
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-08-24 15:10:35 -0300

    backend config: utilizando mysql ao invés de sqlite

--- backend/database/migrations/2025_08_02_182411_create_categories_table.php
+++ backend/database/migrations/2025_08_02_182411_create_categories_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->enum('type', ['INCOME', 'EXPENSE']);
         });

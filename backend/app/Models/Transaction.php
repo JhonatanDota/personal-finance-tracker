914e3dde6cdfe178059dc7a30465780c36bb5313
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-09-02 18:26:34 -0300

    iniciando transactions no backend - add migration, model, factory e seeder

---
+++ backend/app/Models/Transaction.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The maximum length of the description field.
+     * 
+     * @var int
+     */
+    const DESCRIPTION_MAX_LENGTH = 200;
+
+    /**
+     * The minimum value of the value field.
+     * 
+     * @var int
+     */
+    const VALUE_MIN = 0;
+
+    /**
+     * The maximum value of the value field.
+     * 
+     * @var int
+     */
+    const VALUE_MAX = 99999;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_id',
+        'value',
+        'description',
+        'date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'category_id' => 'integer',
+        'value' => 'float',
+        'description' => 'string',
+        'date' => 'date',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
---
+++ backend/database/factories/TransactionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Category;
+use App\Models\Transaction;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::factory(),
+            'value' => $this->faker->randomFloat(min: Transaction::VALUE_MIN, max: Transaction::VALUE_MAX),
+            'description' => $this->faker->text(Transaction::DESCRIPTION_MAX_LENGTH),
+            'date' => $this->faker->date(),
+        ];
+    }
+}

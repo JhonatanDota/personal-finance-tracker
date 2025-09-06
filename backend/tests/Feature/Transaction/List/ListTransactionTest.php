540c7a84ea51b3b2fd94ade13939a8d8285dcbe1
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-09-06 12:43:28 -0300

    adicionando teste para paginação de transactions

--- backend/tests/Feature/Transaction/List/ListTransactionTest.php
+++ backend/tests/Feature/Transaction/List/ListTransactionTest.php
@@ -11,14 +11,14 @@ use App\Http\Resources\Transaction\TransactionResource;
 
 class ListTransactionTest extends TestCase
 {
-    public function testTryListTransactionsNotLogged(): void
+    public function testTryListTransactionsNotLogged()
     {
         $response = $this->json('GET', 'api/transactions/');
 
         $response->assertUnauthorized();
     }
 
-    public function testListCategoriesWithUserWithoutCategories(): void
+    public function testListTransactionsWithUserWithoutTransactions()
     {
         $this->actingAs($this->user);
 
@@ -38,7 +38,7 @@ class ListTransactionTest extends TestCase
         );
     }
 
-    public function testUserCannotListTransactionsFromAnotherUser(): void
+    public function testUserCannotListTransactionsFromAnotherUser()
     {
         $this->actingAs($this->user);
 
@@ -62,7 +62,7 @@ class ListTransactionTest extends TestCase
         ]);
     }
 
-    public function testUserCanViewOwnTransactions(): void
+    public function testUserCanViewOwnTransactions()
     {
         $this->actingAs($this->user);
 
@@ -83,6 +83,63 @@ class ListTransactionTest extends TestCase
                 'last_page' => 1,
                 'per_page' => 15,
                 'total' => 4,
+            ]
+        ]);
+    }
+
+    public function testUserTransactionsPagination()
+    {
+        $this->actingAs($this->user);
+
+        Category::factory(2)
+            ->for($this->user)
+            ->has(Transaction::factory(15))
+            ->create();
+
+        $expectedTotalTransactions = $this->user->transactions()->count();
+        $expectedTransactionsFirstPage = $this->user->transactions()->take(15)->get();
+
+        $response = $this->json('GET', 'api/transactions');
+        $response->assertOk();
+        $response->assertExactJson([
+            'data' => TransactionResource::collection($expectedTransactionsFirstPage)->resolve(),
+            'meta' => [
+                'current_page' => 1,
+                'last_page' => 2,
+                'per_page' => 15,
+                'total' => $expectedTotalTransactions,
+            ]
+        ]);
+
+        $expectedTransactionsSecondPage = $this->user->transactions()->skip(15)->take(15)->get();
+
+        $response = $this->json('GET', 'api/transactions', [
+            'page' => 2
+        ]);
+
+        $response->assertOk();
+        $response->assertExactJson([
+            'data' => TransactionResource::collection($expectedTransactionsSecondPage)->resolve(),
+            'meta' => [
+                'current_page' => 2,
+                'last_page' => 2,
+                'per_page' => 15,
+                'total' => $expectedTotalTransactions,
+            ]
+        ]);
+
+        $response = $this->json('GET', 'api/transactions', [
+            'page' => 3
+        ]);
+
+        $response->assertOk();
+        $response->assertExactJson([
+            'data' => [],
+            'meta' => [
+                'current_page' => 3,
+                'last_page' => 2,
+                'per_page' => 15,
+                'total' => $expectedTotalTransactions,
             ]
         ]);
     }

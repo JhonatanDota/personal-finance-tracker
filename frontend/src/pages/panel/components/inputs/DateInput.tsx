ecde3046fe77ae0ddd450bc8bc669e779755cee6
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-10-01 14:09:40 -0300

    adicionando componente de data

--- frontend/src/pages/panel/components/inputs/DateInput.tsx
+++ frontend/src/pages/panel/components/inputs/DateInput.tsx
@@ -1,29 +1,65 @@
-import { UseFormRegisterReturn } from "react-hook-form";
+import { useState } from "react";
+import { Controller, Control, ControllerRenderProps } from "react-hook-form";
+
+import { DayPicker } from "react-day-picker";
+import Popover from "../popover/Popover";
+import { pt } from "date-fns/locale";
+import "react-day-picker/dist/style.css";
 
 interface DateInputProps {
   label: string;
   placeholder?: string;
-  required?: boolean;
-  register: UseFormRegisterReturn;
+  name: string;
+  control: Control<any>;
   error?: string;
 }
 
-export default function DateInput(props: DateInputProps) {
-  const { label, placeholder, required = false, register, error } = props;
+export default function DateInput({
+  label,
+  placeholder,
+  name,
+  control,
+  error,
+}: DateInputProps) {
+  const [selected, setSelected] = useState<Date>();
+
+  function onSelect(date: Date | undefined, field: ControllerRenderProps) {
+    setSelected(date);
+    field.onChange(date ? date.toISOString().split("T")[0] : "");
+  }
 
   return (
     <div className="input-container">
-      <label className="input-label">
-        {label} {required && "*"}
-      </label>
+      <label className="input-label">{label}</label>
 
-      <input
-        type="date"
-        className="input"
-        placeholder={placeholder}
-        autoComplete="off"
-        {...register}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <Popover
+            trigger={
+              <button type="button" className="input">
+                {selected ? selected.toLocaleDateString() : placeholder}
+              </button>
+            }
+            content={
+              <DayPicker
+                mode="single"
+                locale={pt}
+                selected={selected}
+                className="bg-primary border-[1px] border-secondary text-primary-text p-2 rounded-md"
+                classNames={{
+                  today: "border-success",
+                  selected: "bg-success rounded-lg",
+                  chevron: "fill-tertiary",
+                }}
+                onSelect={(date: Date | undefined) => onSelect(date, field)}
+              />
+            }
+          />
+        )}
       />
+
       {error && <span className="error-message">{error}</span>}
     </div>
   );

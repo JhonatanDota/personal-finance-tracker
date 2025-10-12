b0b191578ddb63b639a685092cfbe75cf74f728d
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-10-12 11:20:57 -0300

    backend transactions: testes para update do category_id

--- backend/tests/Feature/Transaction/UpdateTransactionTest.php
+++ backend/tests/Feature/Transaction/UpdateTransactionTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Transaction;
 
 use Tests\TestCase;
 
+use App\Http\Resources\Transaction\TransactionResource;
+
 use App\Models\Category;
 use App\Models\Transaction;
 
@@ -49,5 +51,102 @@ class UpdateTransactionTest extends TestCase
         $response = $this->json('PATCH', 'api/transactions/' . $transaction->id);
 
         $response->assertOk();
+    }
+
+    // =========================================================================
+    // CATEGORY_ID
+    // =========================================================================
+
+    public function testTryUpdateCategoryIdWithNull()
+    {
+        $this->actingAs($this->user);
+
+        $transaction = Transaction::factory()->for(Category::factory()->for($this->user))->create();
+
+        $response = $this->json('PATCH', 'api/transactions/' . $transaction->id, [
+            'category_id' => null
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonFragment([
+            'category_id' => ['O campo category_id deve ser um inteiro.'],
+        ]);
+    }
+
+    public function testTryUpdateCategoryIdWithUnknownCategory()
+    {
+        $this->actingAs($this->user);
+
+        $transaction = Transaction::factory()->for(Category::factory()->for($this->user))->create();
+
+        $response = $this->json('PATCH', 'api/transactions/' . $transaction->id, [
+            'category_id' => Category::max('id') + 1
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonFragment([
+            'category_id' => ['O valor informado para o campo category_id não existe.'],
+        ]);
+    }
+
+    public function testTryUpdateCategoryIdWithStringValue()
+    {
+        $this->actingAs($this->user);
+
+        $transaction = Transaction::factory()->for(Category::factory()->for($this->user))->create();
+
+        $response = $this->json('PATCH', 'api/transactions/' . $transaction->id, [
+            'category_id' => 'test'
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonFragment([
+            'category_id' => ['O campo category_id deve ser um inteiro.'],
+        ]);
+    }
+
+    public function testTryUpdateCategoryIdWithCategoryFromAnotherUser()
+    {
+        $this->actingAs($this->user);
+
+        $transaction = Transaction::factory()->for(Category::factory()->for($this->user))->create();
+
+        $response = $this->json('PATCH', 'api/transactions/' . $transaction->id, [
+            'category_id' => Category::factory()->create()->id
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonFragment([
+            'category_id' => ['O valor informado para o campo category_id não existe.'],
+        ]);
+    }
+
+    public function testUpdateCategoryIdSuccessfully()
+    {
+        $this->actingAs($this->user);
+
+        $category = Category::factory()->for($this->user)->create();
+        $transaction = Transaction::factory()->for(Category::factory()->for($this->user))->create();
+
+        $this->assertDatabaseHas(Transaction::class, [
+            'id' => $transaction->id,
+            'category_id' => $transaction->category_id,
+        ]);
+
+        $response = $this->json('PATCH', 'api/transactions/' . $transaction->id, [
+            'category_id' => $category->id
+        ]);
+
+        $response->assertOk();
+        $response->assertExactJson(TransactionResource::make(Transaction::find($transaction->id))->resolve());
+
+        $this->assertDatabaseHas(Transaction::class, [
+            'id' => $transaction->id,
+            'category_id' => $category->id,
+        ]);
     }
 }

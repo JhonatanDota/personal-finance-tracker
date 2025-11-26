0f5178ec80cf0c0c5d2422fa610670f6a584de84
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-11-26 18:52:13 -0300

    frontend dashboard: adicionando badge dos filtros ativos

--- frontend/src/pages/panel/sections/dashboard/components/DashboardFilters.tsx
+++ frontend/src/pages/panel/sections/dashboard/components/DashboardFilters.tsx
@@ -3,17 +3,25 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { startOfMonth, endOfMonth, format } from "date-fns";
 
+import { toISOStringBr, parseDate } from "../../../../../utils/date";
+
 import {
   DashboardFilterSchemaType,
   dashboardFilterSchemaData,
 } from "../../../filters/schemas/dashboardFiltersSchema";
 
-import InputContainer from "../../../components/inputs/InputContainer";
-import Label from "../../../components/inputs/Label";
+import FilterOption from "../../../../../types/filterOption";
+
+import { MdOutlineFilterList } from "react-icons/md";
+
 import SectionCard from "../../../components/section/SectionCard";
 import SectionCardTitle from "../../../components/section/SectionCardTitle";
-import { MdOutlineFilterList } from "react-icons/md";
+
+import Label from "../../../components/inputs/Label";
 import DateInput from "../../../components/inputs/DateInput";
+import InputContainer from "../../../components/inputs/InputContainer";
+
+import { FilterBadges } from "../../../components/filter/FilterBadge";
 
 type DashboardFiltersProps = {
   setFilters: (filters: {}) => void;
@@ -39,12 +47,29 @@ export default function DashboardFilters(props: DashboardFiltersProps) {
     setFilters({ dateGe, dateLe });
   }, [dateGe, dateLe]);
 
+  function filterOptionsBuilder(): FilterOption[] {
+    const options = [
+      dateGe && {
+        label: "Data Inicial",
+        value: toISOStringBr(parseDate(dateGe)),
+      },
+      dateLe && {
+        label: "Data Final",
+        value: toISOStringBr(parseDate(dateLe)),
+      },
+    ].filter(Boolean);
+
+    return options as FilterOption[];
+  }
+
   return (
     <SectionCard>
       <SectionCardTitle
         icon={<MdOutlineFilterList className="w-6 h-6 text-success" />}
         title="Filtros"
       />
+
+      <FilterBadges filters={filterOptionsBuilder()} />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
         <InputContainer>
